ba83a2dfc242b599bc5f83e7f3d7575319aa55d9
emersonart <emersonbruno_@hotmail.com>
2019-01-16 16:32:35 -0300

    View de Criar sala

--- application/views/painel/criarSala.php
+++ application/views/painel/criarSala.php
@@ -9,40 +9,64 @@
 
   <div class="card" >
     <div class="card-header" id="dash-professor-card-title">
-      Criar uma sala de Aula
-    </div>
-    <br><br><div class="card-body">
+                <h4>Criar uma Sala</h4>
+              </div>
+    
+    <div class="card-body">
       <div class="row">
-        <div class="col-md-2"></div>
-        <div class="col-md-8">
-           
+        
+        <div class="row">
+        <div class="form-group col-md-8 col-md-offset-2"> 
           <div class="input-group">
-            <span class="input-group-addon" id="basic-addon1"> Nome da turma </span>
-              <input type="text" class="form-control" aria-describedby="basic-addon1" value="">
+            <label> Nome da turma </label>
+              <input type="text" class="form-control" aria-describedby="basic-addon1" name="Nome da Turma" placeholder=" Nome da Turma">
           </div>
+        </div>
+        </div>
+        
+      <div class="row">
+        <div class="form-group col-md-8 col-md-offset-2">
+        <label> Descrição da turma</label>
+        <textarea name="descricaoTurma" rows="3" class="form-control" placeholder="Descrição da Turma"></textarea>
+        </div>
+      </div>
 
-          <br><textarea name="name" rows="3" class="form-control" placeholder="Descrição da Turma"></textarea>
-          
-          <br><div class="input-group">
-            <span class="input-group-addon" id="basic-addon1"> Tempo disponível </span>
-              <input type="date" class="form-control" aria-describedby="basic-addon1" value="">
-          </div>
+      <div class="row">
+        <div class="form-group col-md-8 col-md-offset-2">
+        <div class="input-group">
+          <label class="input-group-addon" id="basic-addon1"> Tempo disponível </label>
+            <input type="date" class="form-control" aria-describedby="basic-addon1" name="tempoTurma">
+        </div>
+        </div>
+      </div>
 
-          <br><div class="input-group-mb-3">
-              <select class="custom-select" id="inputGroupSelect01" name="materia" style="height: 40px; padding-left: 15px;">
+      <div class="row">
+      <div class="form-group col-md-1 col-md-offset-2">
+      <div class="form-group">
+          <select class="custom-select" id="inputGroupSelect01" name="materia" style="height: 40px; padding-left: 15px;">
                 <option selected>Disciplina</option>
                 <option value="1">Física</option>
                 <option value="2">Matemática</option>
                 <option value="3">Português</option>
-              </select>
-          </div>
-          <br>
-          
-          <button type="button" class="btn btn-success"> Criar sala</button>
-          </div>
+          </select>
+        </div>
       </div>
+      </div>
+
+
+      <div class="row">
+        <div class="form-group col-md-2 col-md-offset-2">
+          <button type="button" class="btn btn-success" style="padding-left: 10px;"> Criar sala</button>
+        </div> 
+      </div>
+      
+
+      </div>  
+      
+
     </div>
-  </div>
+    </div>
+    </div>
 
 <?php echo form_close();?>
 
