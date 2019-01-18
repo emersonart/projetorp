f4790e3065e1818a9e87c6a2f390997880c3b347
emersonart <emersonbruno_@hotmail.com>
2019-01-18 19:42:36 -0300

    Update projetorp.sql

--- projetorp.sql
+++ projetorp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 16-Jan-2019 às 04:28
+-- Generation Time: 18-Jan-2019 às 23:37
 -- Versão do servidor: 10.1.37-MariaDB
 -- versão do PHP: 7.2.12
 
@@ -30,14 +30,24 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `tb_activities` (
   `act_id` int(11) NOT NULL,
+  `act_lis_id` int(11) NOT NULL,
   `act_titulo` text NOT NULL,
-  `act_alternativeA` text NOT NULL,
-  `act_alternativeB` text NOT NULL,
-  `act_alternativeC` text NOT NULL,
-  `act_alternativeD` text NOT NULL,
   `act_subject` int(11) NOT NULL,
   `act_teacher` int(11) NOT NULL,
   `act_class` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_answers`
+--
+
+CREATE TABLE `tb_answers` (
+  `ans_id` int(11) NOT NULL,
+  `ans_usu_id` int(11) NOT NULL,
+  `ans_cla_id` int(11) NOT NULL,
+  `ans_lis_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -48,19 +58,23 @@ CREATE TABLE `tb_activities` (
 
 CREATE TABLE `tb_class` (
   `cla_id` int(11) NOT NULL,
+  `cla_nome` text NOT NULL,
   `cla_hash` varchar(22) NOT NULL,
   `cla_teacher` int(11) NOT NULL,
   `cla_subject` int(11) NOT NULL,
   `cla_start_time` varchar(255) NOT NULL,
-  `cla_end_time` varchar(255) NOT NULL
+  `cla_end_time` varchar(255) NOT NULL,
+  `cla_descricao` text NOT NULL,
+  `cla_insc` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Extraindo dados da tabela `tb_class`
 --
 
-INSERT INTO `tb_class` (`cla_id`, `cla_hash`, `cla_teacher`, `cla_subject`, `cla_start_time`, `cla_end_time`) VALUES
-(1, 'fi758d', 1, 1, '14-01-2019', '15-02-2019');
+INSERT INTO `tb_class` (`cla_id`, `cla_nome`, `cla_hash`, `cla_teacher`, `cla_subject`, `cla_start_time`, `cla_end_time`, `cla_descricao`, `cla_insc`) VALUES
+(1, 'Turma 1', 'FI758D', 1, 1, '14-01-2019', '15-02-2019', '', 1),
+(5, 'Turma 2', 'MAC421', 8, 2, '17-01-2019', '18-01-2019', 'adfsadsfadsf', 1);
 
 -- --------------------------------------------------------
 
@@ -74,6 +88,15 @@ CREATE TABLE `tb_configs_site` (
   `con_value` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Extraindo dados da tabela `tb_configs_site`
+--
+
+INSERT INTO `tb_configs_site` (`con_id`, `con_name`, `con_value`) VALUES
+(1, 'site_title', 'Koala Educational'),
+(2, 'site_author', 'Emerson Bruno e Tiago Coutinho'),
+(3, 'site_version', 'Em Desenvolvimento');
+
 -- --------------------------------------------------------
 
 --
@@ -86,7 +109,7 @@ CREATE TABLE `tb_info_users` (
   `inf_name` varchar(155) NOT NULL,
   `inf_lastname` varchar(255) NOT NULL,
   `inf_email` varchar(255) NOT NULL,
-  `inf_registration` int(11) NOT NULL
+  `inf_registration` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -94,9 +117,11 @@ CREATE TABLE `tb_info_users` (
 --
 
 INSERT INTO `tb_info_users` (`inf_id`, `inf_usu_id`, `inf_name`, `inf_lastname`, `inf_email`, `inf_registration`) VALUES
-(1, 1, 'Emerson', 'Bruno', 'emersonbruno_@hotmail.com', 0),
-(2, 3, 'emersonart', 'emersonart', 'emersonbruno_@homail.com', 2147483647),
-(3, 4, 'Emerson', 'Silva', 'emersonbruno2_@homail.com', 2147483647);
+(1, 1, 'Emerson', 'Bruno', 'emersonbruno_@hotmail.com', '0'),
+(2, 3, 'Alyanna', 'emersonart', 'emersonbruno_@homail.com', '20161044110008'),
+(3, 4, 'Emerson', 'Silva', 'emersonbruno2_@homail.com', '2147483647'),
+(6, 8, 'tiago', 'coutinho', 'tiago@coutinho.com', '20161044110009'),
+(10, 13, 'teste de', 'professor', 'professor2@teste.com', '20161044110001');
 
 -- --------------------------------------------------------
 
@@ -128,10 +153,17 @@ INSERT INTO `tb_lists` (`lis_id`, `lis_titulo`, `lis_subject`, `lis_teacher`, `t
 CREATE TABLE `tb_register_class` (
   `reg_id` int(11) NOT NULL,
   `reg_usu_id` int(11) NOT NULL,
-  `reg_cla_hash` int(11) NOT NULL,
+  `reg_cla_hash` varchar(255) NOT NULL,
   `reg_date` varchar(255) NOT NULL,
   `reg_status` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `tb_register_class`
+--
+
+INSERT INTO `tb_register_class` (`reg_id`, `reg_usu_id`, `reg_cla_hash`, `reg_date`, `reg_status`) VALUES
+(1, 8, 'fi758d', '16-01-2019 16:53', 1);
 
 -- --------------------------------------------------------
 
@@ -151,7 +183,8 @@ CREATE TABLE `tb_subjects` (
 --
 
 INSERT INTO `tb_subjects` (`sub_id`, `sub_nome`, `sub_description`, `sub_teacher`) VALUES
-(1, 'fisica', 'materia de fisica', 0);
+(1, 'Física', 'materia de fisica', 1),
+(2, 'Matemática', 'matéria de calculo', 8);
 
 -- --------------------------------------------------------
 
@@ -174,7 +207,9 @@ CREATE TABLE `tb_users` (
 INSERT INTO `tb_users` (`usu_id`, `usu_login`, `usu_password`, `usu_session`, `usu_perm`) VALUES
 (1, 'professor', '123456', '', 2),
 (3, 'emersonart', 'emerson23', '', 0),
-(4, 'emersonart2', 'emerson23', '', 1);
+(4, 'emersonart2', 'emerson23', '', 1),
+(8, 'tiago', 't14g00', '', 2),
+(13, 'professorteste2', 'bhunji741', '', 1);
 
 --
 -- Indexes for dumped tables
@@ -185,6 +220,12 @@ INSERT INTO `tb_users` (`usu_id`, `usu_login`, `usu_password`, `usu_session`, `u
 --
 ALTER TABLE `tb_activities`
   ADD PRIMARY KEY (`act_id`);
+
+--
+-- Indexes for table `tb_answers`
+--
+ALTER TABLE `tb_answers`
+  ADD PRIMARY KEY (`ans_id`);
 
 --
 -- Indexes for table `tb_class`
@@ -239,22 +280,28 @@ ALTER TABLE `tb_activities`
   MODIFY `act_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `tb_answers`
+--
+ALTER TABLE `tb_answers`
+  MODIFY `ans_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `tb_class`
 --
 ALTER TABLE `tb_class`
-  MODIFY `cla_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `cla_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `tb_configs_site`
 --
 ALTER TABLE `tb_configs_site`
-  MODIFY `con_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `con_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `tb_info_users`
 --
 ALTER TABLE `tb_info_users`
-  MODIFY `inf_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `inf_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `tb_lists`
@@ -263,16 +310,22 @@ ALTER TABLE `tb_lists`
   MODIFY `lis_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `tb_register_class`
+--
+ALTER TABLE `tb_register_class`
+  MODIFY `reg_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `tb_subjects`
 --
 ALTER TABLE `tb_subjects`
-  MODIFY `sub_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `sub_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `tb_users`
 --
 ALTER TABLE `tb_users`
-  MODIFY `usu_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `usu_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
