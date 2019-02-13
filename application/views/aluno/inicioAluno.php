335310f80024bb20b90979f1062233742a9d332b
BudgetSole <tiagocoutinho111@gmail.com>
2019-02-13 13:00:37 -0300

    Update Inicio aluno
    Update inicio aluno

--- application/views/aluno/inicioAluno.php
+++ application/views/aluno/inicioAluno.php
@@ -5,6 +5,25 @@
                     <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12 mg-b-15">
                         <div class="hpanel widget-int-shape responsive-mg-b-30">
                             <div class="panel-body">
+                                <div class="white-box">
+                                    <h3 class="box-title">Salas de aula cadastradas:</h3>
+                                    <ul class="basic-list">
+                                        <li>Google Chrome <span class="pull-right label-danger label-1 label">95.8%</span></li>
+                                        <li>Mozila Firefox <span class="pull-right label-purple label-2 label">85.8%</span></li>
+                                        <li>Apple Safari <span class="pull-right label-success label-3 label">23.8%</span></li>
+                                        <li>Internet Explorer <span class="pull-right label-info label-4 label">55.8%</span></li>
+                                        <li>Opera mini <span class="pull-right label-warning label-5 label">28.8%</span></li>
+                                        <li>Mozila Firefox <span class="pull-right label-purple label-6 label">26.8%</span></li>
+                                        <li>Safari <span class="pull-right label-purple label-7 label">31.8%</span></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12 mg-b-15">
+                        <div class="hpanel widget-int-shape responsive-mg-b-30">
+                            <div class="panel-body">
                                 <div class="text-center content-box">
                                     <div class="m icon-box">
                                         <i class="fa fa-users"></i>
@@ -52,6 +71,8 @@
                             </div>
                         </div>
                     </div>
+
+
 
             </div>
 </div>
