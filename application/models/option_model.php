63ad140cab896c2f64f05e6d144a47fe240d277d
emersonart <emersonbruno_@hotmail.com>
2019-02-19 08:52:00 -0300

    Update option_model.php

--- application/models/option_model.php
+++ application/models/option_model.php
@@ -66,12 +66,16 @@ class Option_model extends CI_Model{
 
 
 	public function getMateria($id,$tipo = 0){
-		$this->db->select('*');
-		$this->db->from('tb_subjects');
+		
 
 		if($tipo == 0){
+			$this->db->select('*');
+			$this->db->from('tb_teacher_subject');
+			$this->db->join('tb_subjects','tb_subjects.sub_id = tb_teacher_subject.tea_sub_id and tb_teacher_subject.tea_usu_id','join')
 			$this->db->where('sub_teacher',$id);
 		}else{
+			$this->db->select('*');
+			$this->db->from('tb_subjects');
 			$this->db->where('sub_id',$id);
 		}
 		
