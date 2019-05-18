48260d51ba1cbceaefa5a06ae7eb56a1cc1e50d8
emersonart <emersonbruno_@hotmail.com>
2019-05-18 12:19:49 -0300

    PDF das respostas das listas

--- application/controllers/Pdf.php
+++ application/controllers/Pdf.php
@@ -27,8 +27,73 @@ class Pdf extends CI_Controller {
 		$bootstrap = file_get_contents('./assets/css/bootstrap.min.css');
 		$css = file_get_contents('./assets/style.css');
 		$mpdf = new \Mpdf\Mpdf();
-        $html = $this->load->view('teste/pdf',$dados,true);
+        $html = $this->load->view('pdf/resposta_lista',$dados,true);
         $mpdf->WriteHTML($html);
         $mpdf->Output(); // opens in browser
-	}	
+
+	}
+	
+	public function relatorio_turma($hash = 'FIED32'){
+		$turma = $this->turma->getTurma($hash);
+		$alunos = $this->turma->getAlunos($hash);
+		$listas = $this->questao->getListas($hash);
+		$infos = array();
+		$i = 0;
+		
+		foreach ($alunos as $aluno){
+			$infos[$i]['inf_name'] = $aluno->inf_name;
+			$infos[$i]['inf_lastname'] = $aluno->inf_lastname;
+			$infos[$i]['inf_registration'] = $aluno->inf_registration;
+			//echo $aluno->inf_name." ".$aluno->inf_lastname.": <br>";
+			$j = 0;
+			foreach ($listas as $lista){
+				$infos[$i]['respostas'][$j]['lis_id'] = $lista['lis_id'];
+				$nota = $this->questao->getNotaLista(array('id_lista' => $lista['lis_id'], 'id_aluno'=>$aluno->usu_id));
+				if($nota == ''){
+					$infos[$i]['respostas'][$j]['nota'] = '0';
+				}else{
+					$infos[$i]['respostas'][$j]['nota'] = $nota;
+				}
+				
+
+				//echo "nota: ".$this->questao->getNotaLista(array('id_lista' => $lista['lis_id'], 'id_aluno'=>$aluno->usu_id))."<br>";
+				$j++;
+			}
+			//echo "<hr>";
+			$i++;
+		}
+		echo "<table class='table table-stripped'>
+				<thead>
+					<tr>
+						<td>Aluno</td>";
+						for($k = 0;$k < count($infos[0]['respostas']); $k++) {
+							echo "<td>Lista ".$k."</td>";					
+						}
+		echo "</tr></thead>
+		<tbody>";
+		$l=0;
+		foreach ($infos as $info) {	
+			if($l % 2 == 0){
+				$d = 'background: #eee';
+			}else{
+				$d = '';
+			}
+			echo "<tr style='".$d."'><td>".$info['inf_name'].'</td>';
+			foreach ($info['respostas'] as $resposta) {
+				echo "<td>".$resposta['nota']."</td>";
+			}
+			echo "</tr>";
+			$l++;
+		}
+		echo '</tbody></table>';
+		$dados['notas_aluno'] = $infos;
+		$dados['turma'] = $turma;
+		
+
+		//$mpdf = new \Mpdf\Mpdf();
+        //$html = $this->load->view('teste/boletim_professor',$dados,true);
+        //$mpdf->WriteHTML($html);
+        //$mpdf->Output(); // opens in browser
+	}
+
 }
